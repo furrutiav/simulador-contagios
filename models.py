6668a2f00648aba9a054a95d3904ee065b95d4ff
felipe <felipeurrutia65@gmail.com>
2020-12-13 01:15:09 -0300

    init 2d pipeline and test

--- models.py
+++ models.py
@@ -4,9 +4,55 @@ F. Urrutia V., CC3501, 2020-1
 Modelos:
 """
 
-import libs.easy_shaders as es
+from libs import basic_shapes as bs, transformations as tr, easy_shaders as es, scene_graph as sg
 import numpy as np
 from OpenGL.GL import *
 import random as rd
 from typing import Union
 
+
+class Builder(object):
+    def __init__(self):
+        self._gpu_models = [
+            es.toGPUShape(bs.createColorQuad(0, 1, 0)),
+            es.toGPUShape(bs.createColorQuad(1, 0, 0)),
+            es.toGPUShape(bs.createColorQuad(0, 0, 1))
+        ]
+        self._graph = []
+        for i in range(3):
+            node = sg.SceneGraphNode(f'node_{i}')
+            node.childs += [self._gpu_models[i]]
+
+            self._graph += [node]
+
+    def get_graph(self):
+        return self._graph
+
+
+class Person(object):
+    parameters = {
+        "labels": ["sano", "infectado", "muerto"],
+    }
+
+    def __init__(self, builder):
+
+        self.log_pos = 0, 0
+        self.label = "sano"
+
+        node = sg.SceneGraphNode('person')
+        node.transform = tr.identity()
+        node.childs += [builder.get_graph()[0]]
+
+        self.model = node
+
+    def draw(self, pipeline):
+        glUseProgram(pipeline.shaderProgram)
+        sg.drawSceneGraphNode(self.model, pipeline, transformName='transform')
+
+
+class Background(object):
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
--- view.py
+++ view.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     width, height = 600, 600
     window = glfw.create_window(
-        width, height, 'Snake Game 3D; Autor: F. Urrutia V.', None, None)
+        width, height, 'Simulador Contagios; Autor: F. Urrutia V.', None, None)
 
     if not window:
         glfw.terminate()
@@ -28,20 +28,24 @@ if __name__ == '__main__':
     glfw.set_key_callback(window, controller.on_key)
 
     pipeline_tx_2d = es.SimpleTextureTransformShaderProgram()
+    pipeline_pol_2d = es.SimpleTransformShaderProgram()
 
     glClearColor(8 / 255, 8 / 255, 8 / 255, 1.0)
 
-    glEnable(GL_DEPTH_TEST)
-
+    # glEnable(GL_DEPTH_TEST)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+    b = Builder()
+    p1 = Person(b)
 
     while not glfw.window_should_close(window):
         glfw.poll_events()
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        p1.draw(pipeline_pol_2d)
 
         glfw.swap_buffers(window)
 
